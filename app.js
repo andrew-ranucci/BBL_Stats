557c38fc656dd0a049cff5d179d9cf63ff293ede
andrew-ranucci <andrewr28@vt.edu>
2026-05-31 12:52:16 -0400

    week update

--- app.js
+++ app.js
@@ -352,7 +352,7 @@ async function main() {
     renderMVPLadder([]);
   }
 
-  const CURRENT_WEEK = 3;
+  const CURRENT_WEEK = 4;
   const availablePodcastEpisodes = podcastEpisodes.slice(0, CURRENT_WEEK);
   setupPodcastPlayer(availablePodcastEpisodes);
 
